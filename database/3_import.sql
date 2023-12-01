5423c39fa4bb2deb6b608b51522211d64f341934
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-01 11:36:26 +0200

    Initial commit

--- database/3_import.sql
+++ database/3_import.sql
@@ -5,4 +5,38 @@ INSERT INTO project."user" (id, role_id, username, password, status) VALUES (DEF
 INSERT INTO project."user" (id, role_id, username, password, status) VALUES (DEFAULT, 2, 'rain', '123', 'A');
 INSERT INTO project."user" (id, role_id, username, password, status) VALUES (DEFAULT, 2, 'mitteaktiivne', '123', 'D');
 
-INSERT INTO project.county (
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Harju maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Hiiu maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Ida-Viru maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Jõgeva maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Järva maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Lääne maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Lääne-Viru maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Põlva maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Pärnu maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Rapla maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Saare maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Tartu maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Valga maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Viljandi maakond');
+INSERT INTO project.county (id, name) VALUES (DEFAULT, 'Võru maakond');
+
+INSERT INTO project.material(id, name) VALUES (DEFAULT,'Puit');
+INSERT INTO project.material(id, name) VALUES (DEFAULT,'Metall');
+INSERT INTO project.material(id, name) VALUES (DEFAULT,'Raudbetoon');
+INSERT INTO project.material(id, name) VALUES (DEFAULT,'Muu');
+
+INSERT INTO project.type(id, name) VALUES (DEFAULT, 'Viadukt');
+INSERT INTO project.type(id, name) VALUES (DEFAULT, 'Sild');
+INSERT INTO project.type(id, name) VALUES (DEFAULT, 'Tunnel');
+INSERT INTO project.type(id, name) VALUES (DEFAULT, 'Ökodukt');
+
+INSERT INTO project.location(id, county_id, latitude, longitude) VALUES (DEFAULT, 15, 57.729548, 26.917039);
+INSERT INTO project.location(id, county_id, latitude, longitude) VALUES (DEFAULT, 15, 58.731887, 23.992332);
+INSERT INTO project.location(id, county_id, latitude, longitude) VALUES (DEFAULT, 1, 59.175789, 24.502101);
+INSERT INTO project.location(id, county_id, latitude, longitude) VALUES (DEFAULT, 15, 59.280388, 24.978404);
+
+INSERT INTO project.bridge (id, number, name, length, width, type_id, condition_index, location_id, material_id) VALUES (DEFAULT, 937, 'ALA-RÕUGE', 11.8, 11,2,98,1,1);
+INSERT INTO project.bridge (id, number, name, length, width, type_id, condition_index, location_id, material_id) VALUES (DEFAULT,350,'KASARI',307.80,7.00,2,83,2,3);
+INSERT INTO project.bridge (id, number, name, length, width, type_id, condition_index, location_id, material_id) VALUES (DEFAULT,1310,'KERNU',11.00,20.00,4,83,3,2);
+INSERT INTO project.bridge (id, number, name, length, width, type_id, condition_index, location_id, material_id) VALUES (DEFAULT,150,'VAIDA',132.00,4.00,2,100,4,2);
