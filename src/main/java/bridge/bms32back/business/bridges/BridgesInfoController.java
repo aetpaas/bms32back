1841f8c5fb6a2bcc33a06f79db94c7db1166f9a7
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-05 16:34:46 +0200

    MAAS-26 wip

---
+++ src/main/java/bridge/bms32back/business/bridges/BridgesInfoController.java
@@ -0,0 +1,17 @@
+package bridge.bms32back.business.bridges;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BridgesInfoController {
+
+    @Resource
+    private BridgesInfoService bridgesInfoService;
+
+    @GetMapping("/allbridges")
+    public void getBridgesInfo() {
+        bridgesInfoService.getBridgesInfo
+    }
+}
---
+++ src/main/java/bridge/bms32back/business/bridges/BridgesInfoService.java
@@ -0,0 +1,13 @@
+package bridge.bms32back.business.bridges;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BridgesInfoService {
+
+    @Resource
+    private BridgeInfoService bridgeInfoService;
+
+    public void getAllBridgsInfo();
+}
